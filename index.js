3686d85ed0bf2a1662b96bc7f7da3f32a3ae9f2b
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2023-11-09 17:17:31 +0100

    Add functional submission button

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const openDialog = document.querySelector(".user-input");
+const dialog = document.querySelector(".dialog");
+const cardsContainer = document.querySelector(".cards");
+const submitButton = document.querySelector(".submission");
+const temp = document.querySelector(".temp");
 const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function () {
-    return `${title} by ${author},${pages} pages, ${read}`;
-  };
 }
 function addToLibrary(inputTitle, inputAuthor, inputPages, inputRead) {
   inputTitle = new Book(
@@ -17,10 +19,39 @@ function addToLibrary(inputTitle, inputAuthor, inputPages, inputRead) {
   );
   myLibrary.push(inputTitle);
 }
-let Title = "booktitle";
-let Author = "bookauthor";
-let Pages = "bookpages";
-let Read = "read";
-
-addToLibrary(Title, Author, Pages, Read);
+openDialog.addEventListener("click", () => {
+  dialog.show();
+});
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
+addToLibrary("poop", "poopenheimer", "100", "read");
 console.log(myLibrary);
+submitButton.addEventListener("click", () => {
+  myLibrary.forEach((object, index) => {
+    const tempdiv = document.createElement("div");
+    const temptitle = document.createElement("div");
+    const tempauthor = document.createElement("div");
+    const temppages = document.createElement("div");
+    const tempread = document.createElement("div");
+    tempdiv.appendChild(temptitle);
+    tempdiv.appendChild(tempauthor);
+    tempdiv.appendChild(temppages);
+    tempdiv.appendChild(tempread);
+    temptitle.innerText = object.title;
+    tempauthor.innerText = object.author;
+    temppages.innerText = object.pages;
+    tempread.innerText = object.read;
+    cardsContainer.appendChild(tempdiv);
+  });
+});
+
+// submitButton.addEventListener('click',()=> {
+
+// })
