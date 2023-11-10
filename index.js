ab218b564b10855662ebc6c92b8a72c97746f2c1
Nimueh <139075582+Nimueh-Ilya@users.noreply.github.com>
2023-11-10 18:20:35 +0100

    Fix add book and add delete button

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const openDialog = document.querySelector(".user-input");
 const dialog = document.querySelector(".dialog");
 const cardsContainer = document.querySelector(".cards");
 const submitButton = document.querySelector(".submission");
-const temp = document.querySelector(".temp");
+const bookName = document.querySelector("#book-name");
+const bookAuthor = document.querySelector("#book-author");
+const bookPages = document.querySelector("#book-pages");
+const bookStatus = document.querySelector("#book-status");
 const myLibrary = [];
 function Book(title, author, pages, read) {
   this.title = title;
@@ -22,36 +25,49 @@ function addToLibrary(inputTitle, inputAuthor, inputPages, inputRead) {
 openDialog.addEventListener("click", () => {
   dialog.show();
 });
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-addToLibrary("poop", "poopenheimer", "100", "read");
-console.log(myLibrary);
 submitButton.addEventListener("click", () => {
-  myLibrary.forEach((object, index) => {
-    const tempdiv = document.createElement("div");
-    const temptitle = document.createElement("div");
-    const tempauthor = document.createElement("div");
-    const temppages = document.createElement("div");
-    const tempread = document.createElement("div");
-    tempdiv.appendChild(temptitle);
-    tempdiv.appendChild(tempauthor);
-    tempdiv.appendChild(temppages);
-    tempdiv.appendChild(tempread);
-    temptitle.innerText = object.title;
-    tempauthor.innerText = object.author;
-    temppages.innerText = object.pages;
-    tempread.innerText = object.read;
-    cardsContainer.appendChild(tempdiv);
-  });
+  addToLibrary(
+    bookName.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookStatus.value
+  );
+  const render = function () {
+    cardsContainer.innerHTML = "";
+    myLibrary.forEach((object) => {
+      const tempdiv = document.createElement("div");
+      const temptitle = document.createElement("div");
+      const tempauthor = document.createElement("div");
+      const temppages = document.createElement("div");
+      const tempread = document.createElement("button");
+      const tempdelete = document.createElement("button");
+      tempdiv.dataset.index = myLibrary.indexOf(object);
+
+      tempdiv.appendChild(temptitle);
+      tempdiv.appendChild(tempauthor);
+      tempdiv.appendChild(temppages);
+      tempdiv.appendChild(tempread);
+      tempdiv.appendChild(tempdelete);
+
+      temptitle.innerText = `Title :${object.title}`;
+      tempauthor.innerText = `Author:${object.author}`;
+      temppages.innerText = `${object.pages} pages`;
+      tempread.innerText = object.read;
+      cardsContainer.appendChild(tempdiv);
+      tempdelete.addEventListener("click", () => {
+        myLibrary.splice(tempdiv.dataset.index, 1);
+        render();
+      });
+      tempread.addEventListener("click", () => {
+        console.log(tempdiv.dataset.index);
+        if (object.read == "Read") {
+          object.read = "Not Read";
+        } else {
+          object.read = "Read";
+        }
+        render();
+      });
+    });
+  };
+  render();
 });
-
-// submitButton.addEventListener('click',()=> {
-
-// })
